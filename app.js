325fef88defd1d24b2ffbe1274dccb1ed33c2258
unknown <you@example.com>
2015-06-28 13:02:42 +0300

    start

---
+++ app.js
@@ -0,0 +1,183 @@
+var app = angular.module('myApp', ['angularLoad', 'ngRoute', 'ngAnimate', 'toaster', 'ngSanitize', 'mgcrea.ngStrap']);
+
+
+//document.write('<script src="/modules/post/controllers/PostCtrl.js"><\/script>');
+
+app.config(['$locationProvider', '$routeProvider', '$httpProvider', function ($locationProvider, $routeProvider, $httpProvider) {
+    var modulesPath = 'modules';
+
+    
+        function loadScript(url, callback)
+        {
+            // Adding the script tag to the head as suggested before
+            var head = document.getElementsByTagName('head')[0];
+            var script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src = url;
+
+            // Then bind the event to the callback function.
+            // There are several events for cross browser compatibility.
+            script.onreadystatechange = callback;
+            script.onload = callback;
+
+            // Fire the loading
+            head.appendChild(script);
+        }
+    
+        loadScript("/modules/post/controllers/PostCtrl.js", function(){
+//            angular.element(document).ready(function() {
+//                angular.bootstrap(document, ['myApp']);
+//            });
+        } );
+    
+    
+//alert();
+
+//        setTimeout(function (){
+            
+        
+
+
+    $routeProvider
+    
+    
+
+        .when('/', {
+            templateUrl: modulesPath + '/site/views/main.html'
+        })
+
+        .when('/login', {
+            templateUrl: modulesPath + '/site/views/login.html',
+            controller: 'SiteLogin'
+        })
+
+        .when('/post/published', {
+            templateUrl: modulesPath + '/post/views/index.html',
+            controller: 'PostIndex',
+            resolve: {
+                status: function () {
+                    return 2;
+                }
+            }
+        })
+
+        .when('/post/draft', {
+            templateUrl: modulesPath + '/post/views/index.html',
+            controller: 'PostIndex',
+            resolve: {
+                status: function () {
+                    return 1;
+                }
+            }
+        })
+
+        .when('/post/create', {
+            templateUrl: modulesPath + '/post/views/form.html',
+            controller: 'PostCreate'
+        })
+
+        .when('/post/:id/edit', {
+            templateUrl: modulesPath + '/post/views/form.html',
+            controller: 'PostEdit'
+        })
+
+        .when('/post/:id/delete', {
+            templateUrl: modulesPath + '/post/views/delete.html',
+            controller: 'PostDelete'
+        })
+
+        .when('/post/:id', {
+            templateUrl: modulesPath + '/post/views/view.html',
+            controller: 'PostView'
+        })
+
+        .when('/404', {
+            templateUrl: '404.html'
+        })
+
+        .otherwise({redirectTo: '/404'})
+    ;
+//            console.log($routeProvider);
+//},0);
+//console.log($routeProvider);
+    $locationProvider.html5Mode(true).hashPrefix('!');
+    $httpProvider.interceptors.push('authInterceptor');
+}]);
+
+app.factory('authInterceptor', function ($q, $window) {
+    return {
+        request: function (config) {
+            if ($window.sessionStorage._auth && config.url.substring(0, 4) == 'http') {
+                config.params = {'access-token': $window.sessionStorage._auth};
+            }
+            return config;
+        },
+        responseError: function (rejection) {
+            if (rejection.status === 401) {
+                $window.setTimeout(function () {
+                    $window.location = '/#!/login';
+                }, 1000);
+            }
+            return $q.reject(rejection);
+        }
+    };
+});
+
+app.value('app-version', '0.0.3');
+
+// Need set url REST Api in controller!
+app.service('rest', function ($http, $location, $routeParams) {
+
+    return {
+
+//        baseUrl: 'http://yii2-rest-githubjeka.c9.io/rest/web/',
+        baseUrl: 'http://rest-jeca.net/rest/web/',
+//        baseUrl: 'http://angularjeka.net/rest/web/',
+        path: undefined,
+
+        models: function () {
+            return $http.get(this.baseUrl + this.path + location.search);
+        },
+
+        model: function () {
+            if ($routeParams.expand != null) {
+                return $http.get(this.baseUrl + this.path + "/" + $routeParams.id + '?expand=' + $routeParams.expand);
+            }
+            return $http.get(this.baseUrl + this.path + "/" + $routeParams.id);
+        },
+
+        get: function () {
+            return $http.get(this.baseUrl + this.path);
+        },
+
+        postModel: function (model) {
+            return $http.post(this.baseUrl + this.path, model);
+        },
+
+        putModel: function (model) {
+            return $http.put(this.baseUrl + this.path + "/" + $routeParams.id, model);
+        },
+
+        deleteModel: function () {
+            return $http.delete(this.baseUrl + this.path);
+        }
+    };
+
+});
+
+app
+    .directive('login', ['$http', function ($http) {
+        return {
+            transclude: true,
+            link: function (scope, element, attrs) {
+                scope.isGuest = window.sessionStorage._auth == undefined;
+            },
+
+            template: '<a href="login" ng-if="isGuest">Login</a>'
+        }
+    }])
+    .filter('checkmark', function () {
+        return function (input) {
+            return input ? '\u2713' : '\u2718';
+        };
+    });

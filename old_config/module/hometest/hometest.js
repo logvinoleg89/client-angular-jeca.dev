53fd94ffc5037e21c2dbeb0ee31034266148cf86
unknown <you@example.com>
2015-06-29 22:40:42 +0300

    skeleton6

--- old_config/module/hometest/hometest.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('app.hometest')
-    .config(['featuresResolverProvider', function(featuresResolverProvider) {
-        featuresResolverProvider.localFeatures.push('Unit and e2e testing preconfigured');
-    }]);
